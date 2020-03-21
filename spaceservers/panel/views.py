ddebc4f7d99604cbe24fda818f3ba25bc97d99c1
Ujjwal Malik <ujjwal.malik@hp.com>
2020-03-21 20:18:05 +0530

    check-in template

--- spaceservers/panel/views.py
+++ spaceservers/panel/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, '')
+    return render(request, 'panel/index.html')
+
+
+def mods(request):
+    return render(request, 'panel/mods.html')
